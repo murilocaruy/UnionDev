f804a8e35194c55c0f7429fd3b7910754d59f4bc
murilocaruy <42190902+murilocaruy@users.noreply.github.com>
2019-08-13 10:33:30 -0300

    Add files via upload

---
+++ UnionDev/Controllers/ContaController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UnionDev.Models;
+using System.Data.SqlClient;
+
+namespace UnionDev.Controllers
+{
+    public class ContaController : Controller
+    {
+        SqlConnection con = new SqlConnection();
+        SqlCommand command = new SqlCommand();
+        SqlDataReader dr;
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        void connectionString()
+        {
+                                                                //DEPENDE DO BANCO PARA PROSSEGUIR COM A VALIDAÇÃO
+            con.ConnectionString = @"data source=(nome do servidor); databse=(nome da base) integrated security=(segurança);";
+        }
+
+        [HttpPost]
+        public ActionResult Verifica(Conta conta)
+        {
+            connectionString();
+            con.Open();
+            command.Connection = con;
+            command.CommandText = "SELECT * FROM (TABELA DE LOGIN) WHERE (COLUNA DE LOGIN)='"+conta.Nome+"' AND (COLUNA DE SENHA)='"+conta.Senha+"'";
+            dr = command.ExecuteReader();
+            if (dr.Read())
+            {
+                con.Close();
+                return View("Create");
+            }
+            else
+            {
+                con.Close();
+                return View("Error");
+            }
+
+            
+        }
+    }
+}
---
+++ UnionDev/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UnionDev.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Artigos()
+        {
+            return View();
+        }
+
+        public ActionResult Servico_nr7()
+        {
+            return View();
+        }
+
+        public ActionResult Servico_coaching()
+        {
+            return View();
+        }
+
+        public ActionResult Servico_nr33()
+        {
+            return View();
+        }
+
+        public ActionResult Servico_competencias()
+        {
+            return View();                 
+        }
+
+        public ActionResult Servico_nr35()
+        {
+            return View();
+        }
+
+        public ActionResult Servico_pessoal()
+        {
+            return View();
+        }
+
+    }
+}
